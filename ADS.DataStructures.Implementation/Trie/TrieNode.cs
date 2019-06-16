8e10868142857a2f4b448580eb17603a10e4c128
Rushang Chauhan <chauhanrushang@gmail.com>
2019-06-16 12:40:19 +0530

    Refactored trie node.

--- ADS.DataStructures.Implementation/Trie/TrieNode.cs
+++ ADS.DataStructures.Implementation/Trie/TrieNode.cs
@@ -1,12 +1,13 @@
-﻿using System;
-
-namespace ADS.DataStructure.Implementation.Trie
+﻿namespace ADS.DataStructure.Implementation.Trie
 {
     public class TrieNode
     {
         private static readonly int ALPHABET_SIZE = 26;
-        public TrieNode[] Children;
-        public Boolean IsEndOfWord;
+        private TrieNode[] children;
+        private bool isEndOfWord;
+
+        public TrieNode[] Children { get => children; set => children = value; }
+        public bool IsEndOfWord { get => isEndOfWord; set => isEndOfWord = value; }
 
         public TrieNode()
         {
