be6ac4b989f20240426ed6a62b7e37896dd2525c
Rushang Chauhan <chauhanrushang@gmail.com>
2019-06-30 15:13:41 +0530

    Started Linked lists.

---
+++ ADS.DataStructures.Implementation/LinkedLists/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADS.DataStructure.Implementation.LinkedLists
+{
+    public class Node
+    {
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+
+        public int Data { get; set; }
+        public Node Next { get; set; }
+    }
+}
---
+++ ADS.DataStructures.Implementation/LinkedLists/SinglyLinkedList.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+
+namespace ADS.DataStructure.Implementation.LinkedLists
+{
+    public class SinglyLinkedList
+    {
+        private Node start;
+        public SinglyLinkedList()
+        {
+            start = null;
+        }
+
+        public void InsertAtTheBeginning(int data)
+        {
+            Node temp = new Node(data)
+            {
+                Next = start
+            };
+            start = temp;
+        }
+
+        public void InsertAtTheEnd(int data)
+        {
+
+            if (start == null)
+            {
+                start = new Node(data);
+                return;
+            }
+
+            Node temp = start;
+            while (temp.Next != null)
+            {
+                temp = temp.Next;
+            }
+
+            temp.Next = new Node(data);
+        }
+
+        public void CreateList(IEnumerable<int> data)
+        {
+            foreach (int item in data)
+            {
+                InsertAtTheEnd(item);
+            }
+        }
+
+        public void InsertAfter(int data, int x)
+        {
+            if (start == null)
+            {
+                start = new Node(data);
+                return;
+            }
+
+            Node temp = start;
+
+            while (temp.Next != null)
+            {
+                if (temp.Data == data)
+                {
+                    var node = new Node(data);
+                    node.Next = temp.Next;
+                    temp.Next = node;
+                }
+            }
+
+        }
+
+        public void InsertBefore(int data, int x)
+        {
+
+        }
+
+        public void InsertAtPosition(int data, int p)
+        {
+
+        }
+    }
+}
